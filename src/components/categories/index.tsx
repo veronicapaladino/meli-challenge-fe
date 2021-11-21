637d06871d070762cc689f7b28052faa50cf5e52
Veronica Paladino <veronica.paladino@mercadolibre.com>
2021-11-21 13:25:06 -0300

    se agregan estilos para las categorias

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -7,22 +7,19 @@ interface ICategoriesComponent {
     categories: ICategories[]
 }
 
-const Categories = ({ categories }: ICategoriesComponent) => {
-    return (
-         categories.length > 0 ? <div className="categories-container">
-            {
-                categories[0].path_from_root.map(({ name }) => {
-                    return (
-                        <Breadcrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />} key={name}>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink >{name}</BreadcrumbLink>
-                            </BreadcrumbItem>
-                        </Breadcrumb>
-                    )
-                })
-            }
-        </div> : <h5>No se encontraron categorias para esta busqueda...</h5>
+const Categories = ({ categories }: ICategoriesComponent) => (
+    categories.length > 0 ? 
+    <Breadcrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />}>
+        {
+            categories[0].path_from_root.map(({ name }) => {
+                return (
+                    <BreadcrumbItem key={name}>
+                        <BreadcrumbLink >{name}</BreadcrumbLink>
+                    </BreadcrumbItem>
+                )
+            })
+        }
+    </Breadcrumb> : <h5>No se encontraron categorias para esta busqueda...</h5>
     )
-}
 
 export default Categories;
